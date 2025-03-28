f9151e41d036a9faa60248d8362b8a0e84bcbc5f
unknown <ginger1881@hotmail.com>
2025-03-28 12:21:32 -0500

    feat: first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: MainPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
