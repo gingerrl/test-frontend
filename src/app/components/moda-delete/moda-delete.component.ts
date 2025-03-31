a48ab76b8d9ed3e4527808f14ab1aa234671b531
unknown <ginger1881@hotmail.com>
2025-03-31 15:48:47 -0500

    fix: refactoring of the modal-delete

--- src/app/components/moda-delete/moda-delete.component.ts
+++ src/app/components/moda-delete/moda-delete.component.ts
@@ -1,15 +1,14 @@
 import { Component, EventEmitter, Inject, Output } from '@angular/core';
-import { UserService } from '../../services/user-service.service';
-import { NgEventBus } from 'ng-event-bus';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgEventBus } from 'ng-event-bus';
+import { UserService } from '../../services/user-service.service';
 
 @Component({
   selector: 'app-moda-delete',
   templateUrl: './moda-delete.component.html',
-  styleUrl: './moda-delete.component.css'
+  styleUrl: './moda-delete.component.scss',
 })
 export class ModaDeleteComponent {
-
   @Output() closeModal = new EventEmitter();
 
   constructor(
@@ -17,9 +16,11 @@ export class ModaDeleteComponent {
     private readonly eventBus: NgEventBus,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
+
   onButtonCancel(): void {
     this.closeModal.emit();
   }
+
   onButtonConfirm(): void {
     this.deleteService.delete(this.data.id).subscribe(() => {
       this.eventBus.cast('delete-item');
