4a172ff57eafb225a6694e8088ad831b731e69b9
unknown <ginger1881@hotmail.com>
2025-03-30 20:07:27 -0500

    feat: added config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,63 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModaDeleteComponent } from './components/moda-delete/moda-delete.component';
+import { ModalUserComponent } from './components/modal-user/modal-user.component';
+import { TableListComponent } from './components/table-list/table-list.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { UserService } from './services/user-service.service';
+import { NgEventBus } from 'ng-event-bus';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    TableListComponent,
+    ModalUserComponent,
+    ModaDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatDialogModule
+
+
+
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    UserService,
+    NgEventBus
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
